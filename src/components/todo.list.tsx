7e13dd247fb4e60d39068782e182b2d179a6e95b
Hoang Phuc <hoangphuc311tn@gmail.com>
2026-06-25 09:57:24 +0700

    video_24

---
+++ src/components/todo.list.tsx
@@ -0,0 +1,37 @@
+import TodoData from "./todo.data"
+
+const TodoList = () => {
+    
+    const todos = [
+        {
+            id: 1,
+            title: "Learn React TypeScript",
+            isComplete: false
+        },
+        {
+            id: 2,
+            title: "Subscribe Youtube HoiDanIT",
+            isComplete: true
+        },
+        {
+            id: 3,
+            title: "Learn English",
+            isComplete: true
+        },
+    ]
+
+    return (
+        <>
+            <div>My todo list</div>
+            <br />
+            <TodoData 
+            todos={todos}
+            owner = {"user"}
+            age = {25}
+            isDeveloper = {true}
+            ></TodoData>
+        </>
+    )
+}
+
+export default TodoList
---
+++ src/layout.tsx
@@ -0,0 +1,13 @@
+import TodoList from "./components/todo.list"
+
+function Layout() {
+
+  return (
+    <>
+      <div>Hello Word</div>
+      <TodoList></TodoList>
+    </>
+  )
+}
+
+export default Layout
